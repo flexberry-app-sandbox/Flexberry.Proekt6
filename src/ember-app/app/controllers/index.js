c5f0a7db697c2b622fc682e53a7eae50f77b7921
Flexberry-man <mail@flexberry.net>
2024-01-06 19:05:27 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/index.js
+++ src/ember-app/app/controllers/index.js
@@ -22,67 +22,66 @@ export default Controller.extend({
             link: 'i-i-s-proekt-отч-о-рас-ос-мат-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-отч-о-рас-ос-мат-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-отч-о-рас-ос-мат-l.title'),
-            icon: 'suitcase',
             children: null
           }, {
             link: 'i-i-s-proekt-расх-мат-в-стро-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-расх-мат-в-стро-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-расх-мат-в-стро-l.title'),
-            icon: 'briefcase',
+            icon: 'folder open',
             children: null
           }, {
             link: 'i-i-s-proekt-док-смета-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-док-смета-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-док-смета-l.title'),
-            icon: 'briefcase',
+            icon: 'tasks',
             children: null
           }, {
             link: 'i-i-s-proekt-объект-с-м-р-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-объект-с-м-р-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-объект-с-м-р-l.title'),
-            icon: 'tasks',
+            icon: 'tags',
             children: null
           }, {
             link: 'i-i-s-proekt-ед-измерен-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-ед-измерен-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-ед-измерен-l.title'),
-            icon: 'phone',
+            icon: 'address card',
             children: null
           }, {
             link: 'i-i-s-proekt-от-по-вып-догов-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-от-по-вып-догов-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-от-по-вып-догов-l.title'),
-            icon: 'tasks',
+            icon: 'paperclip',
             children: null
           }, {
             link: 'i-i-s-proekt-номенклатура-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-номенклатура-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-номенклатура-l.title'),
-            icon: 'phone',
+            icon: 'address card',
             children: null
           }, {
             link: 'i-i-s-proekt-спра-должнос-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-спра-должнос-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-спра-должнос-l.title'),
-            icon: 'tags',
+            icon: 'folder',
             children: null
           }, {
             link: 'i-i-s-proekt-док-услов-дог-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-док-услов-дог-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-док-услов-дог-l.title'),
-            icon: 'folder open',
+            icon: 'tasks',
             children: null
           }, {
             link: 'i-i-s-proekt-сотрудники-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-сотрудники-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-сотрудники-l.title'),
-            icon: 'building',
+            icon: 'file',
             children: null
           }, {
             link: 'i-i-s-proekt-спр-контраг-l',
             caption: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-спр-контраг-l.caption'),
             title: i18n.t('forms.application.sitemap.proekt.i-i-s-proekt-спр-контраг-l.title'),
-            icon: 'phone',
+            icon: 'archive',
             children: null
           }, {
             link: 'i-i-s-proekt-т-ч-смета-l',
